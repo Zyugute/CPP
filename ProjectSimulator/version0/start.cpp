2a5d3b68c77a0f22cf3e0614e3df4928356c376e
Zyugute <147388961+Zyugute@users.noreply.github.com>
2025-09-25 08:17:18 +0300

    Update start.cpp

--- ProjectSimulator/version0/start.cpp
+++ ProjectSimulator/version0/start.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-bool Settingsmap(int& x=0,int& y=0,int& varrange=0){
-  return false;
+bool Settingsmap(int& x,int& y,int& varrange){
+  
   //--MAP--
   cout<<"--Map--"<<endl;
   cout<<"(width)x=:";
@@ -12,11 +12,12 @@ bool Settingsmap(int& x=0,int& y=0,int& varrange=0){
   cin>>varrange;
   if ((x<>0) and (y<>0) and (varrange<>0)){
     return true;
-  } 
-  
+  }else{
+    return false;
+  }  
 };
 int main(){
-  bool debugging=false;
+  bool debugging=true;
   int x=0,y=0,varrange=0;
   cout<<"Hello I app for fast create game."<<endl;
   
@@ -24,10 +25,11 @@ int main(){
     cout<<"Let's start with the basic setup."<<endl;
     if(Settingsmap(x,y,varrange)){
       //
+      cout<<"x="<<x<<" y="<<y<<" varrange="<<varrange<<endl;
     }
   }
   else{
     //main.cpp
   }
   return 0;
-};
+}
